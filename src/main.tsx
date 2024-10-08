daea02f9c06659b7a6ee3120e05afd65e994d160
Alyse Kwok <alysekwokk@gmail.com>
2024-10-07 22:46:33 -0400

    Add page structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,50 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { NavBar } from './components/NavBar.tsx';
+import Blog from './pages/Blog.tsx';
+import Resume from './pages/Resume.tsx';
+import Contact from './pages/Contact.tsx';
+
+
+const Layout = () => {
+  return (
+    <>
+    <div>
+      <NavBar/>
+      <Outlet/>
+    </div>
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <App />
+      },
+      {
+        path: "/blog",
+        element: <Blog />
+      },
+      {
+        path: "/resume",
+        element: <Resume />
+      },
+      {
+        path: "/contact",
+        element: <Contact />
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router}/>
 )
